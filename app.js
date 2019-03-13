1fe336885a8ad5405fb42b27d8c7b8c4ebb9bb74
Costa <con.fotiadis@gmail.com>
2019-03-13 13:43:11 +0000

    Hello message.

--- app.js
+++ app.js
@@ -1,6 +1,15 @@
+// load app server using express
 const express = require('express')
 const app = express()
 
+app.get("/", (req, res) => {
+
+  console.log("Responding to route")
+  res.send("Hello from me")
+
+})
+
+// localhost:3003
 app.listen(3003, () => {
-console.log("Server is up and listening");
+  console.log("Starting listening to port 3003")
 })
